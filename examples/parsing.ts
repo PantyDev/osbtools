73c63a2113de59a198f96cc11eecc4e4010a0eb4
PantyDev <pantypon@gmail.com>
2025-01-27 20:17:30 +0200

    feat: update readme

--- examples/parsing.ts
+++ examples/parsing.ts
@@ -1,5 +1,6 @@
 import { Storyboard } from "../src";
 import fs from "fs";
+import { ESbElementProperty } from "../src/types/enums";
 
 const osb = fs.readFileSync(
     './example.osb',
@@ -7,7 +8,7 @@ const osb = fs.readFileSync(
 )
 
 const sb = new Storyboard(osb);
-console.log(sb.getElement(2)?.getProperty(2))
+console.log(sb.getElement(2)?.getProperty<ESbElementProperty.L>(8).data.properties?.getProperty(0))
 
 fs.writeFileSync(
     './example-parsed.osb',
